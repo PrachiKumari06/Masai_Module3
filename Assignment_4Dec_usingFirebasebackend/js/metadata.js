a2940985de28caf26c83ab55f654beba66495e28
Prachi Kumari <prachi.kumari0004@gmail.com>
2025-12-04 22:46:43 +0530

    complete

---
+++ Assignment_4Dec_usingFirebasebackend/js/metadata.js
@@ -0,0 +1,9 @@
+export function navbar(){
+   return `<nav style="background-color: purple; color: white; padding: 10px; margin: 5px; border-radius: 20px;">
+    <a href="index.html" style="margin: 10px;  color:white; text-decoration:none">Home</a>
+        <a href="#" style="margin: 10px;  color:white; text-decoration:none">About</a>
+    <a href="contact.html" style="margin: 10px;  color:white; text-decoration:none">Contact Us</a>
+</nav>` 
+}
+// -----------------------------------Navbar----------------------------------------------------------------------------------
+export const url="https://mini-fullstackapp-masai-4dec-default-rtdb.asia-southeast1.firebasedatabase.app/.json" //this is firebase url:yaha humne.json likha h 
