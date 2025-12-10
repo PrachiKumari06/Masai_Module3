b958ccd58ea172091a4ebbe7ba2be1a614deacfb
Prachi Kumari <prachi.kumari0004@gmail.com>
2025-12-10 12:28:53 +0530

    complete

---
+++ 9Dec_ReactApp/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './component/Navbar.jsx'
+import FirstComponent from './component/FirstComponent.jsx'
+import Content from './component/Content.jsx'
+import Footer from './component/Footer.jsx'
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <FirstComponent />
+      <Content />
+      <Footer />
+    </>
+  )
+}
---
+++ 9Dec_ReactApp/src/component/FirstComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FirstComponent() {
+  return (
+    <div className="first-component">
+      <h2>This is my first React component</h2>
+    </div>
+  )
+}
